11b51f9c73fc24b28f77eef7c018d4b274e69e76
harry1012 <harry1012@foxmail.com>
2025-08-27 08:57:14 +0800

    New commit, clear history

---
+++ js/allpost_data.js
@@ -0,0 +1,440 @@
+window.BLOG_POSTS = [
+  {
+    "title": "极简笔记便签备忘录",
+    "path": "p/49056/",
+    "date": "2017-10-11",
+    "year": 2017,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "工具",
+      "社交"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/MiniNote/MiniNote.png",
+    "excerpt": ""
+  },
+  {
+    "title": "木材材积表",
+    "path": "p/33391/",
+    "date": "2017-10-05",
+    "year": 2017,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "工具",
+      "商业服务"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/mucaicaiji/mucaicaiji.png",
+    "description": "木材材积表,木材体积计算",
+    "excerpt": ""
+  },
+  {
+    "title": "增值税减税降费计算器",
+    "path": "p/2506/",
+    "date": "2017-08-06",
+    "year": 2017,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "金融业"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/shuiwujisuan/shuiwujisuan.png",
+    "description": "增值税减税降费计算器",
+    "excerpt": ""
+  },
+  {
+    "title": "贸小易-CRM客户管理系统",
+    "path": "p/9920/",
+    "date": "2017-04-30",
+    "year": 2017,
+    "categories": [
+      "网站"
+    ],
+    "tags": [
+      "商业服务",
+      "IT科技"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/h5/mxy_crm/mxy_crm1.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "换马加油-虚-拟-货-币走势",
+    "path": "p/7271/",
+    "date": "2019-12-01",
+    "year": 2019,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "金融业",
+      "资讯"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/paomaJiaYou/paomaJiaYou.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "Flick Post-马来西亚",
+    "path": "p/45863/",
+    "date": "2019-06-07",
+    "year": 2019,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "商业服务",
+      "物流服务"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/flickpost/flickpost.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "集果采集商城",
+    "path": "p/6760/",
+    "date": "2019-12-31",
+    "year": 2019,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "电商平台",
+      "商家自营"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/jiguocaiji/jiguocaiji.png",
+    "description": "国内外优质果蔬批发,订购平台",
+    "excerpt": ""
+  },
+  {
+    "title": "ChinaSourcing外贸独立站",
+    "path": "p/23568/",
+    "date": "2016-08-22",
+    "year": 2016,
+    "categories": [
+      "网站"
+    ],
+    "tags": [
+      "商业服务",
+      "电商平台"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/h5/sourcing/0.png",
+    "description": "专业采购解决方案提供商",
+    "excerpt": ""
+  },
+  {
+    "title": "销售管理助手App",
+    "path": "p/23568/",
+    "date": "2016-08-22",
+    "year": 2016,
+    "categories": [
+      "APP"
+    ],
+    "tags": [
+      "商业服务",
+      "IT科技"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/app/WeatherAndroidApp/WeatherAndroidApp.png",
+    "description": "销售管理助手App:App注册授权/数据备份/导出/订单管理/客户管理/商品管理",
+    "excerpt": ""
+  },
+  {
+    "title": "早早云剧-新西兰",
+    "path": "p/33641/",
+    "date": "2020-01-01",
+    "year": 2020,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "电商平台",
+      "文娱"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/zaozaoyunju/zaozaoyunju.png",
+    "description": "新西兰在线剧场-付费视频订阅购买",
+    "excerpt": ""
+  },
+  {
+    "title": "Garbage Classification WeChat Mini Game",
+    "path": "p/58413/",
+    "date": "2020-07-10",
+    "year": 2020,
+    "categories": [
+      "小游戏"
+    ],
+    "tags": [
+      "Entertainment",
+      "Life Services"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/lajifenlei/lajifenlei.png",
+    "description": "Learn garbage classification with a WeChat drop mini game",
+    "excerpt": ""
+  },
+  {
+    "title": "Albert LaCubeTV App-新西兰",
+    "path": "p/48412/",
+    "date": "2018-04-13",
+    "year": 2018,
+    "categories": [
+      "APP"
+    ],
+    "tags": [
+      "社交",
+      "文娱"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/h5/ABGSYUK/ABGSYUK.png",
+    "excerpt": ""
+  },
+  {
+    "title": "大童小意在澳洲",
+    "path": "p/32819/",
+    "date": "2018-03-04",
+    "year": 2018,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "IT科技",
+      "资讯"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/datongxiaoyi/datongxiaoyi.png",
+    "description": "澳洲生活,财经,金融,经济,商业相关的博文频道,澳洲投资理财生活的参考",
+    "excerpt": ""
+  },
+  {
+    "title": "南充云星教育",
+    "path": "p/34349/",
+    "date": "2018-03-06",
+    "year": 2018,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "IT科技",
+      "教育服务"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/nccsStudent/nccsStudent.PNG",
+    "description": "超级课表管理,各班级,各科成绩图表分析,全体通知,申请和审批功能",
+    "excerpt": ""
+  },
+  {
+    "title": "婚礼百科问答",
+    "path": "p/32167/",
+    "date": "2021-09-01",
+    "year": 2021,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "资讯",
+      "生活服务"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/steven/steven.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "新视窗家政服务",
+    "path": "p/62297/",
+    "date": "2021-10-01",
+    "year": 2021,
+    "categories": [
+      "APP"
+    ],
+    "tags": [
+      "商家自营",
+      "生活服务"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/SgXinShiChuang/SgXinShiChuang.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "早早快购-新西兰",
+    "path": "p/11466/",
+    "date": "2021-07-11",
+    "year": 2021,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "电商平台",
+      "旅游服务"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/zaozaokuaigou/zaozaokuaigou.png",
+    "excerpt": ""
+  },
+  {
+    "title": "轻旅圈Lite",
+    "path": "p/53617/",
+    "date": "2022-01-01",
+    "year": 2022,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "社交",
+      "旅游服务"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/outing/outing.png",
+    "excerpt": ""
+  },
+  {
+    "title": "码上点餐",
+    "path": "p/51480/",
+    "date": "2022-02-03",
+    "year": 2022,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "商家自营",
+      "餐饮服务"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/Ordering/Ordering.png",
+    "excerpt": ""
+  },
+  {
+    "title": "Ai克隆数字人 数字人生",
+    "path": "p/39631/",
+    "date": "2025-01-30",
+    "year": 2025,
+    "categories": [
+      "Ai"
+    ],
+    "tags": [
+      "IT科技",
+      "Ai模型"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/shuziren//0.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "Ai证件照合成,图片修改文字,一键换发型",
+    "path": "p/13912/",
+    "date": "2025-05-01",
+    "year": 2025,
+    "categories": [
+      "Ai"
+    ],
+    "tags": [
+      "IT科技"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/app/aizhengjianzhao/0.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "视频批量融帧剪辑特效自动化助手",
+    "path": "p/10229/",
+    "date": "2025-03-02",
+    "year": 2025,
+    "categories": [
+      "软件"
+    ],
+    "tags": [
+      "IT科技"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/2025/shipinrongzhenchajian/0.jpg",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "视频生成字幕Ai助手",
+    "path": "p/36238/",
+    "date": "2024-03-01",
+    "year": 2024,
+    "categories": [
+      "Ai"
+    ],
+    "tags": [
+      "IT科技"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/shipin_zhuan_wenzi/0.svg",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "抖音答题游戏",
+    "path": "p/13912/",
+    "date": "2024-09-01",
+    "year": 2024,
+    "categories": [
+      "小游戏"
+    ],
+    "tags": [
+      "社交",
+      "娱乐"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/app/douyindatixiaoyouxi/0.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "模板海报在线PS",
+    "path": "p/16408/",
+    "date": "2024-11-01",
+    "year": 2024,
+    "categories": [
+      "Ai"
+    ],
+    "tags": [
+      "IT科技"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/app/mubanhaibaopsmini/0.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "扫码点单智慧门店-商家端",
+    "path": "p/53618/",
+    "date": "2023-04-01",
+    "year": 2023,
+    "categories": [
+      "APP"
+    ],
+    "tags": [
+      "商家自营",
+      "生活服务"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/yu_le_dui_mer/0.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "扫码点单智慧门店-推广人",
+    "path": "p/40097/",
+    "date": "2023-02-01",
+    "year": 2023,
+    "categories": [
+      "APP"
+    ],
+    "tags": [
+      "商业服务",
+      "商家自营"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/yu_le_dui_spread/0.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "扫码点单智慧门店-用户端",
+    "path": "p/63671/",
+    "date": "2023-04-01",
+    "year": 2023,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "商家自营",
+      "生活服务"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/wechat/yu_le_dui/0.png",
+    "description": null,
+    "excerpt": ""
+  }
+];
