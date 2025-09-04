455d65b936a0947b03583915d4a9768a4c7ae367
harry1012 <harry1012@foxmail.com>
2025-09-04 15:35:51 +0800

    New commit, clear history

---
+++ js/allpost_data.js
@@ -0,0 +1,17 @@
+window.BLOG_POSTS = [
+  {
+    "title": "正在开发",
+    "path": "p/13912/",
+    "date": "2024-09-01",
+    "year": 2024,
+    "categories": [
+      "小程序"
+    ],
+    "tags": [
+      "工具"
+    ],
+    "cover": "https://ihogu-1251903635.cos.ap-shanghai.myqcloud.com/site/work/app/douyindatixiaoyouxi/0.png",
+    "description": null,
+    "excerpt": ""
+  }
+];
